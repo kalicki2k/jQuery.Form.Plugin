4d6e5b58b602ad8aaaa67314bad4c54a716f2397
Sebastian Kalicki <s.kalicki@bitexpert.de>
2015-10-08 18:51:30 +0200

    Revised fillSelect method

--- jquery.form.js
+++ jquery.form.js
@@ -10,132 +10,144 @@
     'use strict';
 
     $.fn.form = function () {
-        var me = this;
-
-        var form = {
-            clear: function () {
-                $(me).find('input, textarea')
-                    .not('input:checkbox, input:radio, input:submit')
-                    .each(function (index, input) {
-                        $(input).val(null);
-                    });
-
-                $(me).find('input:checkbox, input:radio')
-                    .each(function (index, input) {
-                        $(input).prop('checked', $(input).is('[checked]'));
-                    });
-
-                $(me).find('select')
-                    .each(function (index, select) {
-                        $('option', select).each(function (index, option) {
-                            $(option).prop('selected', $(option).is('[selected]'));
+        var me = this,
+            form = {
+                clear: function () {
+                    $(me).find('input, textarea')
+                        .not('input:checkbox, input:radio, input:submit')
+                        .each(function (index, input) {
+                            $(input).val(null);
                         });
-                    });
 
-                return this;
-            },
-            clearElement: function (selector) {
-                var element = $(selector, me);
-
-                switch (element[0].nodeName.toLowerCase()) {
-                    case 'input':
-                    case 'textarea':
-                        element.val(null);
-                        break;
-                    case 'select':
-                        $('option', element).each(function (index, option) {
-                            $(option).prop('selected', $(option).is('[selected]'));
+                    $(me).find('input:checkbox, input:radio')
+                        .each(function (index, input) {
+                            $(input).prop('checked', $(input).is('[checked]'));
                         });
-                        break;
-                }
 
-                return this;
-            },
-            fill: function (data) {
-                $.each(data, function (name, value) {
-                    var element = $('[name="' + name + '"]', me);
+                    $(me).find('select')
+                        .each(function (index, select) {
+                            $('option', select).each(function (index, option) {
+                                $(option).prop('selected', $(option).is('[selected]'));
+                            });
+                        });
+
+                    return this;
+                },
+                clearElement: function (selector) {
+                    var element = $(selector, me);
 
                     switch (element[0].nodeName.toLowerCase()) {
                         case 'input':
                         case 'textarea':
-                            form.fillInput(element, value);
+                            element.val(null);
                             break;
                         case 'select':
-                            form.fillSelect(element, value);
+                            $('option', element).each(function (index, option) {
+                                $(option).prop('selected', $(option).is('[selected]'));
+                            });
                             break;
                     }
-                });
 
-                return this;
-            },
-            fillInput: function (selector, value) {
-                var element = $(selector, me);
+                    return this;
+                },
+                fill: function (data) {
+                    $.each(data, function (name, value) {
+                        var element = $('[name="' + name + '"]', me);
 
-                switch (typeof (value)) {
-                    case 'boolean':
-                        element.prop('checked', value);
-                        break;
-                    default:
-                        element.val(value);
-                }
-
-                return this;
-            },
-            fillSelect: function (selector, data, keys) {
-                var element = $(selector, me);
-
-                if ('undefined' === typeof (keys)) {
-                    keys = [].concat('value', 'title');
-                }
-
-                $.each(data, function (index, value) {
-                    var option = $('<option>', {
-                        value: value[keys[0]],
-                        html: value[keys[1]]
-                    });
-                    if ('selected' === value.selected || true === value.selected) {
-                        option.attr('selected', 'selected');
-                    }
-                    option.appendTo(element, me);
-                });
-
-                return this;
-            },
-            read: function (callback) {
-
-                var selectedData = {};
-
-                $(me).find('input, textarea')
-                    .not('input:checkbox, input:radio, input:submit')
-                    .each(function (index, input) {
-                        if (input.value.length) {
-                            selectedData[input.name] = input.value;
+                        switch (element[0].nodeName.toLowerCase()) {
+                            case 'input':
+                            case 'textarea':
+                                form.fillInput(element, value);
+                                break;
+                            case 'select':
+                                form.fillSelect(element, value);
+                                break;
                         }
                     });
 
-                $(me).find('input:checkbox:checked, input:radio:checked')
-                    .each(function (index, input) {
-                        selectedData[input.name] = input.value;
-                    });
+                    return this;
+                },
+                fillInput: function (selector, value) {
+                    var element = $(selector, me);
 
-                $(me).find('select')
-                    .each(function (index, select) {
-                        $('option:selected', select)
-                            .each(function (index, option) {
-                                if ('' !== option.value) {
-                                    selectedData[select.name] = option.value;
-                                }
-                            });
-                    });
+                    switch (typeof (value)) {
+                        case 'boolean':
+                            element.prop('checked', value);
+                            break;
+                        default:
+                            element.val(value);
+                    }
 
-                if (callback && typeof (callback) === 'function') {
-                    var response = (!$.isEmptyObject(selectedData)) ? selectedData : false;
-                    callback(response);
+                    return this;
+                },
+                fillSelect: function (selector, data, keys) {
+
+                    switch (typeof (data)) {
+                        case 'object':
+                            _createOptions(selector, data, keys)
+                            break;
+                        default:
+                            form.fillInput(selector, data);
+                            break;
+                    }
+
+
+                    return this;
+                },
+                read: function (callback) {
+
+                    var selectedData = {};
+
+                    $(me).find('input, textarea')
+                        .not('input:checkbox, input:radio, input:submit')
+                        .each(function (index, input) {
+                            if (input.value.length) {
+                                selectedData[input.name] = input.value;
+                            }
+                        });
+
+                    $(me).find('input:checkbox:checked, input:radio:checked')
+                        .each(function (index, input) {
+                            selectedData[input.name] = input.value;
+                        });
+
+                    $(me).find('select')
+                        .each(function (index, select) {
+                            $('option:selected', select)
+                                .each(function (index, option) {
+                                    if ('' !== option.value) {
+                                        selectedData[select.name] = option.value;
+                                    }
+                                });
+                        });
+
+                    if (callback && typeof (callback) === 'function') {
+                        var response = (!$.isEmptyObject(selectedData)) ? selectedData : false;
+                        callback(response);
+                    }
+
+                    return this;
                 }
+            };
 
-                return this;
+        function _createOptions(selector, data, keys) {
+            var element = $(selector, me);
+
+            if ('undefined' === typeof (keys)) {
+                keys = [].concat('value', 'title');
             }
-        };
+
+            $.each(data, function (index, value) {
+                var option = $('<option>', {
+                    value: value[keys[0]],
+                    html: value[keys[1]]
+                });
+                if ('selected' === value.selected || true === value.selected) {
+                    option.attr('selected', 'selected');
+                }
+                option.appendTo(element, me);
+            });
+        }
 
         return form;
     };
